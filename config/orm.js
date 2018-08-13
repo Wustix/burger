a3edc8e80c7ca7a43bd2855e461f8a7cee0dfc7b
unknown <wroehrman@yahoo.com>
2018-08-12 21:23:01 -0500

    updates to burger files

---
+++ config/orm.js
@@ -0,0 +1,23 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+var orm = {
+
+    all: function() {
+
+    },
+    create: function() {
+
+    },
+    update: function() {
+
+    }
+};
+
+
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
