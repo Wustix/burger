e57a305959bde14af23231b9760204d7a09a085d
unknown <wroehrman@yahoo.com>
2018-08-12 20:34:39 -0500

    created files for burger

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Royal with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Farmer Burger');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Bacon and Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger with Onion Ring');
